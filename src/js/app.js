0c7f42b6d3d6dadb36b2bae8655921982324684f
BryanStevie <bpalomino425@gmail.com>
2022-05-03 18:29:59 -0500

    Modificación de header - slider - section

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,56 @@
-console.log('hola desde la consola');
+window.addEventListener('load', function(){
+
+var slider = new Glider(document.querySelector('.carousel-lista'), {
+        slidesToShow: 1,
+        dots: '.carousel-indicadores',
+        draggable: false,
+        arrows: {
+          prev: '.carousel-anterior',
+          next: '.carousel-siguiente'
+        }
+      });
+
+/* AUTO REPRODUCCIÓN DE SLIDER INICIO */
+slideAutoPaly(slider, '.carousel-lista');
+
+function slideAutoPaly(glider, selector, delay = 5000, repeat = true) {
+    let autoplay = null;
+    const slidesCount = glider.track.childElementCount;
+    let nextIndex = 1;
+    let pause = true;
+
+    function slide() {
+        autoplay = setInterval(() => {
+            if (nextIndex >= slidesCount) {
+                if (!repeat) {
+                    clearInterval(autoplay);
+                } else {
+                    nextIndex = 0;
+                }
+            }
+            glider.scrollItem(nextIndex++);
+        }, delay);
+    }
+
+    slide();
+
+    var element = document.querySelector(selector);
+    element.addEventListener('mouseover', (event) => {
+        if (pause) {
+            clearInterval(autoplay);
+            pause = false;
+        }
+    }, 300);
+
+    element.addEventListener('mouseout', (event) => {
+        if (!pause) {
+            slide();
+            pause = true;
+        }
+    }, 300);
+}
+/* AUTO REPRODUCCIÓN DE SLIDER FIN */
+
+
+
+});
